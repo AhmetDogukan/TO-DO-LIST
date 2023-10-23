0560b23884dad9dad52e0842bcbe899b98c84f88
AhmetDogukan <92661348+AhmetDogukan@users.noreply.github.com>
2023-10-23 23:39:51 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+const input = document.querySelector('input')
+const btn = document.querySelector('button')
+const wrapper = document.getElementById('wrapper')
+const alert = document.querySelector('.alert')
+
+input.onkey = function(e){
+    if(e.keyCode == 13){
+        kaydet()
+    }
+}
+
+function kaydet(){
+    let value = input.value;
+    if(value.trim() != ''){
+        let div = document.createElement('div')
+        div.style.display = 'flex'
+        div.style.justifyContent = 'space-between'
+        div.style.alignItems = 'center'
+        div.style.padding = '15px'
+        div.addEventListener('click', ciz)
+
+        let p = document.createElement('p')
+        p.style.margin = '0'
+
+        let span = document.createElement('span')
+
+        p.textContent = value
+
+        span.innerHTML = '<i class="fa-solid fa-trash"></i>'
+        span.addEventListener('click', sil)
+
+        div.appendChild(p)
+        div.appendChild(span)
+        wrapper.appendChild(div)
+    }else{
+        alert.classList.add('d-block')
+        alert.classList.remove('d-none')
+        setTimeout(timeOut, 1000)
+    }
+    input.value = ""
+}
+
+function ciz(){
+    this.classList.toggle('bg-secondary')
+    this.classList.toggle('text-white')
+    this.children[0].classList.toggle('text-decoration-line-through')
+}
+
+function sil(){
+    this.parentElement.remove()
+}
+
+function timeOut(){
+    alert.classList.remove('d-block')
+    alert.classList.add('d-none')
+}
